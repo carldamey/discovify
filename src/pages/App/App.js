a75dfe7c57f32c2a6c7a6ac9468ee8059ca7c18e
carldamey <carldamey@gmail.com>
2023-12-19 14:14:37 -0800

    migrates api calls to dedicated utility file

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -7,50 +7,23 @@ import Navbar from "../../components/NavBar/Navbar"
 import SearchArea from "../../components/SearchArea/SearchArea"
 import ResultsArea from "../../components/ResultsArea/ResultsArea"
 
-const spotifyAPI = new SpotifyWebApi()
-
-function getTokenFromUrl() {
-	return window.location.hash
-		.substring(1)
-		.split("&")
-		.reduce((initial, item) => {
-			let parts = item.split("=")
-			initial[parts[0]] = decodeURIComponent(parts[1])
-			return initial
-		}, {})
-}
-
 function App() {
 	const [token, setToken] = useState("")
 	const [nowPlaying, setNowPlaying] = useState({})
 	const [loggedIn, setLoggedin] = useState(false)
 
 	useEffect(() => {
-		console.log("token from url:", getTokenFromUrl())
-		const spotifyToken = getTokenFromUrl().access_token
+		console.log("token from url:", discovifyAPI.getTokenFromUrl())
+		const spotifyToken = discovifyAPI.getTokenFromUrl().access_token
 		window.location.hash = ""
 		console.log("token:", spotifyToken)
 
 		if (spotifyToken) {
 			setToken(spotifyToken)
-			spotifyAPI.setAccessToken(spotifyToken)
-			spotifyAPI.getMe().then((user) => {
-				console.log(user)
-			})
-			//use api
+			discovifyAPI.setAccessToken(spotifyToken)
 			setLoggedin(true)
 		}
 	})
-
-	function getNowPlaying() {
-		spotifyAPI.getMyCurrentPlaybackState().then((response) => {
-			console.log(response)
-			setNowPlaying({
-				name: response.item.name,
-				albumArt: response.item.album.images[0].url,
-			})
-		})
-	}
 
 	return (
 		<div className="App">
@@ -66,7 +39,7 @@ function App() {
 				</>
 			)}
 			{loggedIn && (
-				<button onClick={() => getNowPlaying()}>Check Now Playing</button>
+				<button onClick={() => {discovifyAPI.getNowPlaying().then(data => setNowPlaying(data))}}>Check Now Playing</button>
 			)}
 		</div>
 	)
--- src/utilities/discovify-api.js
+++ src/utilities/discovify-api.js
@@ -1,28 +1,65 @@
 import SpotifyWebApi from "spotify-web-api-js"
-const spotify = SpotifyWebApi()
+const spotifyAPI = new SpotifyWebApi()
 
 const methods = {
-  getToken: getTokenFromUrl,
-  getTopSongs,
-}
-
-try {
-  spotify.setAccessToken(getTokenFromUrl.access_token)
-} catch(err) {
-  console.log("token err", err)
+	getTokenFromUrl,
+	setAccessToken,
+	getNowPlaying,
 }
 
 function getTokenFromUrl() {
-	return window.location.hash.substring(1).split("&").reduce((initial, item) => {
-		let parts = item.split("=")
-		initial[parts[0]] = decodeURIComponent(parts[1])
-		return initial
-	}, {})
+	return window.location.hash
+		.substring(1)
+		.split("&")
+		.reduce((initial, item) => {
+			let parts = item.split("=")
+			initial[parts[0]] = decodeURIComponent(parts[1])
+			return initial
+		}, {})
 }
 
-function getTopSongs() {
-  // console.log(spotify.getMyTopTracks())
-  console.log("hi")
+function setAccessToken(spotifyToken) {
+	spotifyAPI.setAccessToken(spotifyToken)
+	spotifyAPI.getMe().then((user) => {
+		console.log(user)
+	})
+	//use api
+}
+
+// function getNowPlaying() {
+// 	spotifyAPI.getMyCurrentPlaybackState().then((response) => {
+// 		console.log("now playing response:", response)
+// 		console.log("name:", response.item.name)
+// 		console.log(typeof response.item.name)
+// 		return {
+// 			name: response.item.name,
+// 			albumArt: response.item.album.images[0].url,
+// 		}
+// 	})
+// }
+
+function getNowPlaying() {
+  return new Promise((resolve, reject) => {
+    spotifyAPI.getMyCurrentPlaybackState().then((response) => {
+      resolve({
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  })
+	spotifyAPI.getMyCurrentPlaybackState().then((response) => {
+		console.log("now playing response:", response)
+		console.log("name:", response.item.name)
+		console.log(typeof response.item.name)
+		return {
+			name: response.item.name,
+			albumArt: response.item.album.images[0].url,
+		}
+	})
 }
 
 export default methods
+
