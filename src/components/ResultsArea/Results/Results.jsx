53308b74020195edda803128136075d739712558
carldamey <carldamey@gmail.com>
2023-12-19 22:44:40 -0800

    changes css, minor changes to components

--- src/components/ResultsArea/Results/Results.jsx
+++ src/components/ResultsArea/Results/Results.jsx
@@ -2,6 +2,19 @@ export default function Results() {
   return(
     <div className="Results">
       Results
+      <ul>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        <li>song</li>
+        
+      </ul>
     </div>
   )
 }
