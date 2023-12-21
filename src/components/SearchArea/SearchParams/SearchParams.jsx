f7841636b95b919f267a9c574434935aeadae9f5
carldamey <carldamey@gmail.com>
2023-12-20 16:49:09 -0800

    refactors sliders to contain form in div

--- src/components/SearchArea/SearchParams/SearchParams.jsx
+++ src/components/SearchArea/SearchParams/SearchParams.jsx
@@ -1,10 +1,102 @@
-import SliderSection from "./SliderSection/SliderSection.jsx"
+import "./SearchParams.css"
+import genres from "./genres"
 
 export default function SearchParams() {
 	return (
 		<div className="SearchParams">
-			<h1>Search Params</h1>
-			<SliderSection />
+			<form>
+				<div className="sliders">
+					<div className="slider">
+						<label>
+							POPULARITY
+							<input type="range"></input>
+						</label>
+					</div>
+					<div className="slider">
+						<label>
+							ENERGY
+							<input type="range"></input>
+						</label>
+					</div>
+					<div className="slider">
+						<label>
+							TEMPO
+							<input type="range"></input>
+						</label>
+					</div>
+					<div className="slider">
+						<label>
+							MOOD
+							<input type="range"></input>
+						</label>
+					</div>
+					<div className="slider">
+						<label>
+							DANCEABILITY
+							<input type="range"></input>
+						</label>
+					</div>
+					<div className="slider">
+						<label>
+							ACOUSTICNESS
+							<input type="range"></input>
+						</label>
+					</div>
+				</div>
+
+				<div className="dropdowns">
+					<label>
+						GENRE
+						<div className="dropdown">
+							<select>
+								{[...genres.slice(1)].map((genre) => {
+									return <option value={genre}>{genre.toUpperCase()}</option>
+								})}
+							</select>
+						</div>
+					</label>
+					<label>
+						GENRE
+						<div className="dropdown">
+							<select>
+								{genres.map((genre) => {
+									return <option value={genre}>{genre.toUpperCase()}</option>
+								})}
+							</select>
+						</div>
+					</label>
+					<label>
+						GENRE
+						<div className="dropdown">
+							<select>
+								{genres.map((genre) => {
+									return <option value={genre}>{genre.toUpperCase()}</option>
+								})}
+							</select>
+						</div>
+					</label>
+					<label>
+						GENRE
+						<div className="dropdown">
+							<select>
+								{genres.map((genre) => {
+									return <option value={genre}>{genre.toUpperCase()}</option>
+								})}
+							</select>
+						</div>
+					</label>
+					<label>
+						GENRE
+						<div className="dropdown">
+							<select>
+								{genres.map((genre) => {
+									return <option value={genre}>{genre.toUpperCase()}</option>
+								})}
+							</select>
+						</div>
+					</label>
+				</div>
+			</form>
 		</div>
 	)
 }
