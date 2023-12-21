47eaa18aa60754abf053c7bcfd1c6532fed7cac8
carldamey <carldamey@gmail.com>
2023-12-20 19:32:06 -0800

    successfully allows search form to be filled

--- src/components/SearchArea/SearchParams/SearchParams.jsx
+++ src/components/SearchArea/SearchParams/SearchParams.jsx
@@ -1,9 +1,9 @@
 import "./SearchParams.css"
 import genres from "./genres"
-import { useState } from "react"
+import {useState} from "react"
+import discovifyAPI from "../../../utilities/discovify-api"
 
 export default function SearchParams() {
-	
 	const [formData, setFormData] = useState({
 		popularity: 0,
 		energy: 0,
@@ -15,55 +15,115 @@ export default function SearchParams() {
 		genre2: "",
 		genre3: "",
 		genre4: "",
-		genre5: ""
+		genre5: "",
 	})
 
 	function handleSliderChange(e) {
 		const {name, value} = e.target
 		setFormData((oldFormData) => ({
 			...oldFormData,
-			[name]: parseFloat(value)
+			[name]: parseFloat(value),
 		}))
+	}
+
+	function handleDropdownChange(e) {
+		const {name, value} = e.target
+		setFormData((oldFormData) => ({
+			...oldFormData,
+			[name]: value,
+		}))
+	}
+
+	function handleSearch(e) {
+		e.preventDefault()
+		console.log(formData)
 	}
 
 	return (
 		<div className="SearchParams">
-			<form>
+			<form onSubmit={handleSearch}>
 				<div className="sliders">
 					<div className="slider">
 						<label>
 							POPULARITY
-							<input name="popularity" type="range" min={0} max={100} onChange={handleSliderChange}></input>
+							<input
+								name="popularity"
+								type="range"
+								min={0}
+								max={100}
+								value={formData.popularity}
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 					<div className="slider">
 						<label>
 							ENERGY
-							<input name="energy" type="range" min={0} max={1} step="0.01" onChange={handleSliderChange}></input>
+							<input
+								name="energy"
+								type="range"
+								min={0}
+								max={1}
+								value={formData.energy}
+								step="0.01"
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 					<div className="slider">
 						<label>
 							TEMPO
-							<input name="tempo" type="range" min={0} max={1} step="0.01" onChange={handleSliderChange}></input>
+							<input
+								name="tempo"
+								type="range"
+								min={0}
+								max={1}
+								value={formData.tempo}
+								step="0.01"
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 					<div className="slider">
 						<label>
 							MOOD
-							<input name="valence" type="range" min={0} max={1} step="0.01" onChange={handleSliderChange}></input>
+							<input
+								name="valence"
+								type="range"
+								min={0}
+								max={1}
+								value={formData.valence}
+								step="0.01"
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 					<div className="slider">
 						<label>
 							DANCEABILITY
-							<input name="danceability" type="range" min={0} max={1} step="0.01" onChange={handleSliderChange}></input>
+							<input
+								name="danceability"
+								type="range"
+								min={0}
+								max={1}
+								value={formData.danceability}
+								step="0.01"
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 					<div className="slider">
 						<label>
 							ACOUSTICNESS
-							<input name="acousticness" type="range" min={0} max={1} step="0.01" onChange={handleSliderChange}></input>
+							<input
+								name="acousticness"
+								type="range"
+								min={0}
+								max={1}
+								value={formData.acousticness}
+								step="0.01"
+								onChange={handleSliderChange}
+							></input>
 						</label>
 					</div>
 				</div>
@@ -72,7 +132,11 @@ export default function SearchParams() {
 					<label>
 						GENRE
 						<div className="dropdown">
-							<select>
+							<select
+								name="genre1"
+								value={formData.genre1}
+								onChange={handleDropdownChange}
+							>
 								{[...genres.slice(1)].map((genre) => {
 									return <option value={genre}>{genre.toUpperCase()}</option>
 								})}
@@ -82,7 +146,11 @@ export default function SearchParams() {
 					<label>
 						GENRE
 						<div className="dropdown">
-							<select>
+							<select
+								name="genre2"
+								value={formData.genre2}
+								onChange={handleDropdownChange}
+							>
 								{genres.map((genre) => {
 									return <option value={genre}>{genre.toUpperCase()}</option>
 								})}
@@ -92,7 +160,11 @@ export default function SearchParams() {
 					<label>
 						GENRE
 						<div className="dropdown">
-							<select>
+							<select
+								name="genre3"
+								value={formData.genre3}
+								onChange={handleDropdownChange}
+							>
 								{genres.map((genre) => {
 									return <option value={genre}>{genre.toUpperCase()}</option>
 								})}
@@ -102,7 +174,11 @@ export default function SearchParams() {
 					<label>
 						GENRE
 						<div className="dropdown">
-							<select>
+							<select
+								name="genre4"
+								value={formData.genre4}
+								onChange={handleDropdownChange}
+							>
 								{genres.map((genre) => {
 									return <option value={genre}>{genre.toUpperCase()}</option>
 								})}
@@ -112,7 +188,11 @@ export default function SearchParams() {
 					<label>
 						GENRE
 						<div className="dropdown">
-							<select>
+							<select
+								name="genre5"
+								value={formData.genre5}
+								onChange={handleDropdownChange}
+							>
 								{genres.map((genre) => {
 									return <option value={genre}>{genre.toUpperCase()}</option>
 								})}
